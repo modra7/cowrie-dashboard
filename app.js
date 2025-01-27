0a56885208a1787508585b4e2eafac1a9e64b3b7
example <modra71717@gmail.com>
2025-01-27 14:24:00 +0100

    Fixed module loading

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const { Honeypot } = require('../models'); // Adjust the path as necessary
+var Honeypot = require('./models');
 
 
 var indexRouter = require('./routes/index');
